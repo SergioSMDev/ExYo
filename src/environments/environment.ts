59e922c7052233c3341a4ed815b5e6d4edf553aa
Sergey <56604489+SergioSMDev@users.noreply.github.com>
2019-12-02 14:11:45 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,13 +13,7 @@ export const environment = {
             'https://identitytoolkit.googleapis.com/v1/accounts:delete?key=',
     },
     firebaseConfig: {
-        apiKey: 'AIzaSyCPGOjrEl6_UhjQbKLYeb8vlOGOfjDSh8w',
-        authDomain: 'itaka-9db8e.firebaseapp.com',
-        databaseURL: 'https://itaka-9db8e.firebaseio.com',
-        projectId: 'itaka-9db8e',
-        storageBucket: 'itaka-9db8e.appspot.com',
-        messagingSenderId: '135417569373',
-        appId: '1:135417569373:web:3b4c6268e6df8658',
+      
     },
 };
 
